84440ce637a06a40887257308019bdd744554898
jorgejc <jorgejc25@gmail.com>
2024-09-28 06:34:17 -0500

    Se realizan función listar por Id en el módulo de inventarios

--- router/inventario.js
+++ router/inventario.js
@@ -142,4 +142,17 @@ router.get('/', async function (req, res) {
 })
 
 
+router.get('/:inventarioId', async function(req, res){
+    try {
+        const inventario = await Inventario.findById(req.params.inventarioId);
+        if(!inventario) {
+            return res.status(404).send('Inventario no existe');
+        }
+        res.send(inventario);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send('Ocurrió un error al consultar inventario');
+    }
+});
+
 module.exports = router
